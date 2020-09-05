2101bd892fbd33bf3b4e7cb7c80361c9473cde5e
Aditya Sah <67780629+adityasah1501@users.noreply.github.com>
2020-09-05 15:46:03 +0530

    timer.py

--- timer.py
+++ timer.py
@@ -1,7 +1,7 @@
 import time
 
-for hour in range (0,100):
-	for minute in range (0,600):
-		for second in range (0,600):
+for hour in range (0,24):
+	for minute in range (0,60):
+		for second in range (0,60):
 			print("{}:{}:{}". format(hour,minute,second))
-			time.sleep(0.00000000000000000000000000000000001)
+			time.sleep(1)
